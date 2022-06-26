91b0d8577f5c090960da9c31f3a10d0b83c3fb8f
koki-develop <kou.pg.0131@gmail.com>
2022-06-26 11:45:44 +0900

    file 保存用のバケットを作成

---
+++ terraform/s3.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "files" {
+  bucket = "github-stats-files"
+}
+
+resource "aws_s3_bucket_public_access_block" "files" {
+  bucket = aws_s3_bucket.files.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
