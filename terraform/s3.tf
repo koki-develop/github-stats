98d968e0e9da7a4dee12ddb850fdc8b1c84bf703
koki-develop <kou.pg.0131@gmail.com>
2022-06-27 18:09:02 +0900

    frontend 用の s3 バケットを作成

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,3 +10,16 @@ resource "aws_s3_bucket_public_access_block" "files" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "frontend" {
+  bucket = "github-stats-frontend"
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
