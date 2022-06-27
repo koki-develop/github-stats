4f4929f0541c9c7db00bf0b26d8da9f5d67ce99d
koki-develop <kou.pg.0131@gmail.com>
2022-06-27 21:20:01 +0900

    フロントエンド配信用の CloudFront を作成

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,57 @@
+resource "aws_cloudfront_distribution" "frontend" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    origin_id   = aws_s3_bucket.frontend.id
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.frontend.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id       = aws_s3_bucket.frontend.id
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0 # TODO: 適切に設定する
+    default_ttl            = 0 # TODO: 適切に設定する
+    max_ttl                = 0 # TODO: 適切に設定する
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.frontend.arn
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  custom_error_response {
+    error_code         = "403"
+    response_code      = "404"
+    response_page_path = "/404/index.html"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "frontend" {}
+
+resource "aws_cloudfront_function" "frontend" {
+  name    = "${local.prefix}-frontend"
+  runtime = "cloudfront-js-1.0"
+  publish = true
+  code    = file("${path.module}/functions/src/frontend.js")
+}
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,7 @@
+#
+# files
+#
+
 resource "aws_s3_bucket" "files" {
   bucket = "${local.prefix}-files"
 }
@@ -11,6 +15,10 @@ resource "aws_s3_bucket_public_access_block" "files" {
   restrict_public_buckets = true
 }
 
+#
+# frontend
+#
+
 resource "aws_s3_bucket" "frontend" {
   bucket = "${local.prefix}-frontend"
 }
@@ -22,4 +30,21 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = data.aws_iam_policy_document.frontend.json
+}
+
+data "aws_iam_policy_document" "frontend" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.frontend.iam_arn]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.frontend.arn}/*"]
+  }
 }
