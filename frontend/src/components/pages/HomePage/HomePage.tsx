3391c2df9cfc2f25e93945c3398f11fce886bc76
koki-develop <kou.pg.0131@gmail.com>
2022-06-28 09:15:35 +0900

    とりあえず今日と昨日のデータを見れるようにした

--- frontend/src/components/pages/HomePage/HomePage.tsx
+++ frontend/src/components/pages/HomePage/HomePage.tsx
@@ -1,7 +1,78 @@
 import { NextPage } from "next";
+import { Language } from "../../../models/language";
 
-const HomePage: NextPage = () => {
-  return <div>hello world</div>;
+export type HomePageProps = {
+  users: {
+    updatedAt: string;
+    today: number;
+    yesterday: number;
+  };
+  orgs: {
+    updatedAt: string;
+    today: number;
+    yesterday: number;
+  };
+  repos: {
+    updatedAt: string;
+    today: number;
+    yesterday: number;
+  };
+  topLanguages: {
+    updatedAt: string;
+    today: Language[];
+    yesterday: Language[];
+  };
+};
+
+const HomePage: NextPage<HomePageProps> = (props) => {
+  const { users, orgs, repos, topLanguages } = props;
+
+  return (
+    <div>
+      <div>
+        <h1>users</h1>
+        <p>updated: {users.updatedAt}</p>
+        <p>today: {users.today}</p>
+        <p>yesterday: {users.yesterday}</p>
+      </div>
+      <div>
+        <h1>orgs</h1>
+        <p>updated: {orgs.updatedAt}</p>
+        <p>today: {orgs.today}</p>
+        <p>yesterday: {orgs.yesterday}</p>
+      </div>
+      <div>
+        <h1>repos</h1>
+        <p>updated: {repos.updatedAt}</p>
+        <p>today: {repos.today}</p>
+        <p>yesterday: {repos.yesterday}</p>
+      </div>
+      <div>
+        <h1>top languages</h1>
+        <p>updated: {topLanguages.updatedAt}</p>
+        <div>
+          today
+          <ol>
+            {topLanguages.today.map((language) => (
+              <li key={language.name}>
+                {language.name} ({language.type}): {language.count}
+              </li>
+            ))}
+          </ol>
+        </div>
+        <div>
+          yesterday
+          <ol>
+            {topLanguages.yesterday.map((language) => (
+              <li key={language.name}>
+                {language.name} ({language.type}): {language.count}
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,38 @@
-import HomePage from "../components/pages/HomePage";
+import { GetStaticProps } from "next";
+import { Language } from "../models/language";
+import HomePage, { HomePageProps } from "../components/pages/HomePage";
+import data from "../data.json";
 
 export default HomePage;
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+  const [
+    { users, orgs, repos, languages },
+    { users: yUsers, orgs: yOrgs, repos: yRepos, languages: yLanguages },
+  ] = data;
+
+  return {
+    props: {
+      users: {
+        updatedAt: users.date,
+        today: users.count,
+        yesterday: yUsers.count,
+      },
+      orgs: {
+        updatedAt: orgs.date,
+        today: orgs.count,
+        yesterday: yOrgs.count,
+      },
+      repos: {
+        updatedAt: repos.date,
+        today: repos.count,
+        yesterday: yRepos.count,
+      },
+      topLanguages: {
+        updatedAt: languages.date,
+        today: languages.languages.slice(0, 10) as Language[],
+        yesterday: yLanguages.languages.slice(0, 10) as Language[],
+      },
+    },
+  };
+};
