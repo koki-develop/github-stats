b2dcb51362cb05be794c4d87cd6a19541f49cb94
koki-develop <kou.pg.0131@gmail.com>
2022-06-28 21:48:05 +0900

    言語の推移が見れるようにした

--- frontend/src/components/pages/RepositoriesPage/RepositoriesPage.tsx
+++ frontend/src/components/pages/RepositoriesPage/RepositoriesPage.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import Highcharts from "highcharts";
+import Highcharts, { SeriesLineOptions } from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { Language } from "../../../models/language";
 import { useMemo } from "react";
@@ -40,15 +40,57 @@ const RepositoriesPage: NextPage<RepositoriesPageProps> = (props) => {
       xAxis: {
         categories: seriesCategories,
       },
-      series: [
-        { type: "line", name: "Public Repositories", data: series, yAxis: 0 },
-      ],
+      series: [{ type: "line", name: "Public Repositories", data: series }],
     };
     return options;
   }, []);
+
+  const languagesOptions = useMemo(() => {
+    const reversed = languages.concat().reverse();
+
+    const categories = reversed.map((row) =>
+      format(new Date(row.date), "yyyy-MM-dd")
+    );
+
+    const m = new Map<string, number[]>([]);
+    for (const row of languages) {
+      for (const language of row.languages) {
+        const data = m.get(language.name);
+        if (data) {
+          m.set(language.name, [...data, language.count]);
+        } else {
+          m.set(language.name, [language.count]);
+        }
+      }
+    }
+    const series: SeriesLineOptions[] = Array.from(m.entries()).map(
+      ([name, data]) => {
+        return {
+          type: "line",
+          name,
+          data,
+        };
+      }
+    );
+
+    const options: Highcharts.Options = {
+      title: { text: null },
+      yAxis: { title: { text: null } },
+      xAxis: { categories },
+      series: series,
+    };
+    return options;
+  }, []);
+
   return (
     <div>
-      <HighchartsReact highcharts={Highcharts} options={reposOptions} />
+      <div>
+        <HighchartsReact highcharts={Highcharts} options={reposOptions} />
+      </div>
+
+      <div>
+        <HighchartsReact highcharts={Highcharts} options={languagesOptions} />
+      </div>
     </div>
   );
 };
