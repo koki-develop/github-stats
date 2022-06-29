f331ecb247ca87e86ff24de4efce9edf6cd279f2
koki-develop <kou.pg.0131@gmail.com>
2022-06-29 20:34:34 +0900

    LineChart コンポーネントを RepositoriesChart に適用

--- frontend/src/components/pages/RepositoriesPage/RepositoriesChart.tsx
+++ frontend/src/components/pages/RepositoriesPage/RepositoriesChart.tsx
@@ -1,7 +1,5 @@
-import { format } from "date-fns";
 import React, { memo, useMemo } from "react";
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
+import LineChart from "../../utils/LineChart";
 
 export type RepositoriesChartProps = {
   data: {
@@ -13,40 +11,14 @@ export type RepositoriesChartProps = {
 const RepositoriesChart: React.FC<RepositoriesChartProps> = memo((props) => {
   const { data } = props;
 
-  const formattedData = useMemo(() => {
-    const ascData = data
-      .concat()
-      .reverse()
-      .map((row) => ({
-        date: format(new Date(row.date), "yyyy-MM-dd"),
-        count: row.count,
-      }));
-    const lastData = ascData.slice(-2);
-    if (lastData[0].date === lastData[1].date) {
-      ascData.splice(-2, 1);
-    }
-    return ascData;
+  const points = useMemo(() => {
+    return data.map((row) => ({
+      date: row.date,
+      data: [{ name: "Public Repositories", value: row.count }],
+    }));
   }, [data]);
 
-  const xAxisCategories: string[] = useMemo(() => {
-    return formattedData.map((row) => row.date);
-  }, [formattedData]);
-
-  const seriesData: number[] = useMemo(() => {
-    return formattedData.map((row) => row.count);
-  }, [formattedData]);
-
-  const options = useMemo(() => {
-    const options: Highcharts.Options = {
-      title: { text: null },
-      yAxis: { title: { text: null } },
-      xAxis: { categories: xAxisCategories },
-      series: [{ type: "line", name: "Public Repositories", data: seriesData }],
-    };
-    return options;
-  }, [xAxisCategories, seriesData]);
-
-  return <HighchartsReact highcharts={Highcharts} options={options} />;
+  return <LineChart points={points} />;
 });
 
 RepositoriesChart.displayName = "RepositoriesChart";
