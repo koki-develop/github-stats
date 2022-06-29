007aabfcd5bb37db188be51e63580b749415494e
koki-develop <kou.pg.0131@gmail.com>
2022-06-30 08:37:22 +0900

    PieChart コンポーネントを作成

--- frontend/src/components/pages/RepositoriesPage/RepositoriesPage.tsx
+++ frontend/src/components/pages/RepositoriesPage/RepositoriesPage.tsx
@@ -1,11 +1,9 @@
 import { NextPage } from "next";
-import Highcharts, { PointOptionsObject, SeriesLineOptions } from "highcharts";
-import HighchartsReact from "highcharts-react-official";
 import { Language } from "../../../models/language";
 import RepositoriesLineChart from "./RepositoriesLineChart";
 import { useMemo } from "react";
-import { format } from "date-fns";
 import LanguagesLineChart from "./LanguagesLineChart";
+import PieChart from "../../utils/PieChart";
 
 export type RepositoriesPageProps = {
   repos: {
@@ -21,81 +19,13 @@ export type RepositoriesPageProps = {
 const RepositoriesPage: NextPage<RepositoriesPageProps> = (props) => {
   const { repos, languages } = props;
 
-  const languagesOptions = useMemo(() => {
-    const reversed = languages.concat().reverse();
-
-    const categories = reversed.map((row) =>
-      format(new Date(row.date), "yyyy-MM-dd")
-    );
-
-    const m = new Map<string, number[]>([]);
-    for (const row of languages) {
-      for (const language of row.languages) {
-        const data = m.get(language.name);
-        if (data) {
-          m.set(language.name, [...data, language.count]);
-        } else {
-          m.set(language.name, [language.count]);
-        }
-      }
-    }
-    const series: SeriesLineOptions[] = Array.from(m.entries()).map(
-      ([name, data]) => {
-        return {
-          type: "line",
-          name,
-          data,
-        };
-      }
-    );
-
-    const options: Highcharts.Options = {
-      title: { text: null },
-      yAxis: { title: { text: null } },
-      xAxis: { categories },
-      series: series,
-    };
-    return options;
-  }, []);
-
-  const languagePieOptions = useMemo(() => {
-    const repoCount = repos[0].count;
+  const languagesPieData = useMemo(() => {
     const todayLanguages = languages[0].languages;
-    const topLanguages = todayLanguages.slice(0, 15);
-    const topLanguagesTotalPercentage =
-      (topLanguages.reduce<number>((prev, current) => {
-        return prev + current.count;
-      }, 0) /
-        repoCount) *
-      100;
-    const otherPercentage =
-      Math.floor((100 - topLanguagesTotalPercentage) * 100) / 100;
-
-    const data: PointOptionsObject[] = topLanguages.map((language) => {
-      return {
-        name: language.name,
-        y: Math.floor((language.count / repoCount) * 100 * 100) / 100,
-        color: language.color ?? "#000000",
-      };
-    });
-    data.push({
-      name: "Other",
-      y: otherPercentage,
-      color: "#888888",
-    });
-
-    const options: Highcharts.Options = {
-      chart: { type: "pie" },
-      title: { text: null },
-      series: [
-        {
-          type: "pie",
-          name: "Public Repositories",
-          data,
-        },
-      ],
-    };
-    return options;
+    return todayLanguages.map((language) => ({
+      name: language.name,
+      value: language.count,
+      color: language.color,
+    }));
   }, []);
 
   return (
@@ -109,7 +39,7 @@ const RepositoriesPage: NextPage<RepositoriesPageProps> = (props) => {
       </div>
 
       <div>
-        <HighchartsReact highcharts={Highcharts} options={languagePieOptions} />
+        <PieChart total={repos[0].count} data={languagesPieData} />
       </div>
     </div>
   );
---
+++ frontend/src/components/utils/PieChart.tsx
@@ -0,0 +1,61 @@
+import Highcharts, { PointOptionsObject, SeriesLineOptions } from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import React, { useMemo } from "react";
+
+export type PieChartProps = {
+  total: number;
+  data: {
+    name: string;
+    value: number;
+    color?: string;
+  }[];
+};
+
+const floor = (num: number) => Math.floor(num * 100) / 100;
+
+const PieChart: React.FC<PieChartProps> = (props) => {
+  const { total, data } = props;
+
+  const formattedData = useMemo(() => {
+    const formattedData = data
+      .map((row) => ({
+        name: row.name,
+        color: row.color ?? "#000000",
+        percentage: floor((row.value / total) * 100),
+      }))
+      .reduce((prev, current) => {
+        if (current.percentage < 0.5) {
+          return prev;
+        }
+        return [...prev, current];
+      }, []);
+
+    const totalPercentage = formattedData.reduce<number>((prev, current) => {
+      return prev + current.percentage;
+    }, 0);
+    formattedData.push({
+      name: "Other",
+      color: "#888888",
+      percentage: floor(100 - totalPercentage),
+    });
+    return formattedData;
+  }, []);
+
+  const options = useMemo(() => {
+    const seriesData = formattedData.map((row) => ({
+      name: row.name,
+      color: row.color ?? "#000000",
+      y: row.percentage,
+    }));
+
+    const options: Highcharts.Options = {
+      title: { text: null },
+      series: [{ type: "pie", name: "Public Repositories", data: seriesData }],
+    };
+    return options;
+  }, []);
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
+};
+
+export default PieChart;
