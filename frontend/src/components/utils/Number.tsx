08b3bd560ab60ca7033aa9f50afdd6f86c213dae
koki-develop <kou.pg.0131@gmail.com>
2022-07-03 14:23:38 +0900

    Number コンポーネントを作成

---
+++ frontend/src/components/utils/Number.tsx
@@ -0,0 +1,35 @@
+import classNames from "classnames";
+import React, { memo, useMemo } from "react";
+
+export type NumberProps = {
+  className?: string;
+  diff?: boolean;
+  value: number;
+};
+
+const Number: React.FC<NumberProps> = memo((props) => {
+  const { className, diff, value } = props;
+
+  const op = useMemo(() => {
+    if (!diff) return null;
+    if (value === 0) return <>&plusmn;</>;
+    if (value > 0) return <>&#043;</>;
+    return null;
+  }, [value]);
+
+  return (
+    <span
+      className={classNames(className, {
+        "text-red-600": diff && value < 0,
+        "text-green-600": diff && value > 0,
+      })}
+    >
+      {op}
+      {value.toLocaleString()}
+    </span>
+  );
+});
+
+Number.displayName = "Number";
+
+export default Number;
