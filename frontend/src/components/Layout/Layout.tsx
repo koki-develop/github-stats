ed41201c282349ecd05e21dff7e0b147e4509cef
koki-develop <kou.pg.0131@gmail.com>
2022-07-03 13:09:55 +0900

    ヘッダとフッタを作成

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { memo } from "react";
 
 export type LayoutProps = {
@@ -9,7 +10,30 @@ const Layout: React.FC<LayoutProps> = memo((props) => {
 
   return (
     <div>
-      <div>{children}</div>
+      <header>
+        <h1 className=" text-4xl font-bold">GitHub Stats</h1>
+      </header>
+
+      <main className="py-4 px-2">{children}</main>
+
+      <footer className="flex flex-col items-center justify-center">
+        <small>&copy;2022 koki sato</small>
+
+        <ul className="flex flex-col items-center">
+          <li>
+            <a
+              href="https://github.com/koki-develop/github-stats"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              View on GitHub
+            </a>
+          </li>
+          <li>
+            <Link href="/privacy">Privacy Policy</Link>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 });
