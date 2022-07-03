ec84ef9c7dd858be2c784f82db17a82f3ac2508c
koki-develop <kou.pg.0131@gmail.com>
2022-07-03 21:16:04 +0900

    gtag の処理を追加

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const rendered = useRef<boolean>(false);
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    if (!rendered.current) {
+      rendered.current = true;
+      return;
+    }
+    window.gtag("config", process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+      page_path: pathname,
+      debug_mode: process.env.NEXT_PUBLIC_STAGE !== "production",
+    });
+  }, [pathname]);
+
   return <Component {...pageProps} />;
 };
 
