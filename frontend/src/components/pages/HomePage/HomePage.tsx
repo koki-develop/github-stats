2a9e333ce77ac9161fa8446a0b1b35091d75aef6
koki-develop <kou.pg.0131@gmail.com>
2022-07-03 11:34:26 +0900

    最新の言語一覧のデータを表示

--- frontend/src/components/pages/HomePage/HomePage.tsx
+++ frontend/src/components/pages/HomePage/HomePage.tsx
@@ -6,24 +6,47 @@ export type HomePageProps = {
     latest: {
       date: string;
       count: number;
+      diff: number;
     };
-    diff: number;
     data: { date: string; count: number }[];
+  };
+
+  languages: {
+    latest: {
+      date: string;
+      topLanguages: {
+        name: string;
+        color: string;
+        count: number;
+        diff: number;
+      }[];
+    };
   };
 };
 
 const HomePage: NextPage<HomePageProps> = (props) => {
-  const { repos } = props;
+  const { repos, languages } = props;
 
   return (
     <div>
       <div>
         <div>
-          Public Repository {repos.latest.count} ({repos.diff})
+          Public Repository {repos.latest.count} ({repos.latest.diff})
         </div>
         <div>updated: {repos.latest.date}</div>
         <div>
           <RepositoriesLineChart repos={repos.data} />
+        </div>
+        <div>
+          <div>Languages</div>
+          <div>updated: {languages.latest.date}</div>
+          <div>
+            {languages.latest.topLanguages.map((language) => (
+              <div key={language.name}>
+                {language.name}: {language.count} ({language.diff})
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ import data from "../data.json";
 export default HomePage;
 
 export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+  // repositories
   const reposData = data.map((record) => record.repos);
   const reposSortedData = reposData
     .concat()
@@ -12,12 +13,43 @@ export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
   const [reposTodayData, reposYesterdayData] = reposSortedData;
   const reposDiff = reposTodayData.count - reposYesterdayData.count;
 
+  // languages
+  const languagesData = data.map((record) => record.languages);
+  const languagesSortedData = languagesData
+    .concat()
+    .sort((a, b) => (a.date < b.date ? 1 : -1));
+  const [languagesTodayData, languagesYesterdayData] = languagesSortedData;
+  const languagesTodayTopData = languagesTodayData.languages
+    .concat()
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 50);
+
   return {
     props: {
       repos: {
-        latest: reposTodayData,
+        latest: {
+          ...reposTodayData,
+          diff: reposDiff,
+        },
         diff: reposDiff,
         data: reposSortedData,
+      },
+      languages: {
+        latest: {
+          date: languagesTodayData.date,
+          topLanguages: languagesTodayTopData.map((language) => ({
+            name: language.name,
+            color: language.color ?? "#000000",
+            count: language.count,
+            diff: (() => {
+              const yesterdayLanguage = languagesYesterdayData.languages.find(
+                (yesterdayLanguage) => yesterdayLanguage.name === language.name
+              );
+              if (!yesterdayLanguage) return language.count;
+              return language.count - yesterdayLanguage.count;
+            })(),
+          })),
+        },
       },
     },
   };
